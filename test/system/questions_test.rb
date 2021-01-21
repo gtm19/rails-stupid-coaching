3998d3da5b0715a04d21845e5fc6f4f9fd84e357
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-21 19:55:53 +0000

    Completed optional testing

---
+++ test/system/questions_test.rb
@@ -0,0 +1,17 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "ask form renders" do
+    visit ask_url
+    assert_selector "input#question"
+    has_button? "Ask"
+  end
+
+  test "asking question elicits puzzled response from coach" do
+    visit ask_url
+    fill_in "question", with: "Who are you?"
+    click_on "Ask"
+
+    assert_selector "blockquote.coach-answer", text: "Silly question, get dressed and go to work!"
+  end
+end
